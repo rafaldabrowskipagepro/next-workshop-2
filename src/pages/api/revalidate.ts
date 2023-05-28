69fe9142ff99dcb938b0d96674d6b7b3bc480a6d
Rafal Dabrowski <rafal.dabrowski@pagepro.pl>
2023-05-28 23:55:25 +0200

    Initial commit

--- src/pages/api/revalidate.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from "next";
-
-const handler: NextApiHandler = async (_, res) => {
-  await res.revalidate("/rendering/ssg-with-manual-revalidation");
-
-  res.end();
-};
-
-export default handler;
